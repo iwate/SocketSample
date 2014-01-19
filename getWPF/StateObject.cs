7a7af94e7d9d13c956412874d51f50781f95228a
papeMK2 <papemk2@gmail.com>
2014-01-19 19:03:10 +0900

    完成

---
+++ getWPF/StateObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace getWPF
+{
+    class StateObject
+    {
+        public Socket workSocket;
+        public const int BufferSize = 256;
+        public byte[] buffer = new byte[BufferSize];
+        public StringBuilder sb = new StringBuilder();
+    }
+}
